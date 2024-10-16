e52bf8862a8e4a22c6a9c1d167a8a650e7286da4
bárbara augusto <lonnysferrari@gmail.com>
2024-10-16 16:13:51 -0300

    ok

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ let posY = 50;
 
 const moveSpeed = 50;
 const originalBackgroundColor = '#f8b71d';
+
+document.addEventListener('keydown', (event)=>{
+    switch(event.code){
+        case 'ArrowUp':
+        if (posX > 0) 
+            posY = posY - moveSpeed;
+            break;
+        case 'ArrowDown':
+            if (posY < window.innerHeight - circle.offsetHeight)
+                posY = posY + moveSpeed;
+    }
+})
