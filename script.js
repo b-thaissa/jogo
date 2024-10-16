35b6d4220a13c00aa773f585ea5470436aded667
bárbara augusto <lonnysferrari@gmail.com>
2024-10-16 15:50:55 -0300

    ok

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 const circle = document.getElementById("circle");
 const gameArea = document.getElementById("gameArea");
+
+let posX = 50;
+let posY = 50;
+
+const moveSpeed = 50;
+const originalBackgroundColor = '#f8b71d';
